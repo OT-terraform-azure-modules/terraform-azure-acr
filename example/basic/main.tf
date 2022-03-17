c4ddacf684c009670d318e303d25befee3704ac2
S Aishwarya <s.aishwarya@opstree.com>
2022-03-17 10:40:45 +0000

    Update example/basic/main.tf

--- example/basic/main.tf
+++ example/basic/main.tf
@@ -1,9 +1,31 @@
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+provider "azurerm" {
+  features {}
+}
+module "res_group" {
+  source                  = "OT-terraform-azure-modules/resource-group/azure"
+  resource_group_name     = "example-rg"
+  resource_group_location = "east us"
+  tag_map = {
+    Name = "AzureResourceGroup"
+  }
 }
 
 module "acr" {
-    source = "../"
-    resource_group_name = module.res_group.name
-    
+  source                  = "../"
+  container_registry_name = "examplexmpregistry"
+  resource_group_name     = module.res_group.resource_group_name
+  acr_location            = "east us"
+  sku                     = "Basic"
+  admin_enabled           = false
+  public_network_access   = true
+  trust_policy            = false
+  zone_redundancy_enabled = false
+  georeplications         = []
+  retention               = null
+  identity                = "SystemAssigned"
+  identity_id             = null
+  encryption              = null
+  key_vault_key_id        = null
+  identity_client_id      = null
+  lock_level_value        = ""
 }
