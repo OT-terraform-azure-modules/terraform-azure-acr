94ff78c0f29007fc8c27a6c0fe41bbb6bed0b3f5
S Aishwarya <s.aishwarya@opstree.com>
2022-03-21 06:04:32 +0000

    Replace main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
 }
+######################## Azure Container Registry ##############################
 
 resource "azurerm_container_registry" "ContainerRegistry" {
   name                          = var.container_registry_name
@@ -10,6 +11,10 @@ resource "azurerm_container_registry" "ContainerRegistry" {
   admin_enabled                 = var.admin_enabled
   public_network_access_enabled = var.public_network_access
   zone_redundancy_enabled       = var.zone_redundancy_enabled
+  data_endpoint_enabled         = var.sku == "Premium" ? var.data_endpoint_enabled : false
+  anonymous_pull_enabled        = var.sku == "Premium" || var.sku == "Standard" ? var.anonymous_pull_enabled : false
+  quarantine_policy_enabled     = var.sku == "Premium" ? var.quarantine_policy_enabled : false
+  network_rule_bypass_option    = var.network_rule_bypass_option
   tags                          = var.tag_map
 
   dynamic "georeplications" {
@@ -30,17 +35,19 @@ resource "azurerm_container_registry" "ContainerRegistry" {
   }
 
   dynamic "trust_policy" {
-    for_each = var.trust_policy != null && var.sku == "Premium" && var.encryption != "true" ? ["enabled"] : []
+    for_each = var.trust_policy != null && var.sku == "Premium" && var.encryption == "false" ? ["enabled"] : []
     content {
       enabled = var.trust_policy
     }
   }
+
   identity {
     type         = var.identity
     identity_ids = var.identity == "UserAssigned" ? [var.identity_id] : []
   }
+
   dynamic "encryption" {
-    for_each = var.encryption != null && var.sku == "Premium" ? [var.encryption] : []
+    for_each = var.encryption != false && var.sku == "Premium" ? [var.encryption] : []
     content {
       enabled            = var.encryption
       key_vault_key_id   = var.key_vault_key_id
@@ -48,6 +55,48 @@ resource "azurerm_container_registry" "ContainerRegistry" {
     }
   }
 }
+
+############################## Private Endpoint ###############################
+
+resource "azurerm_private_endpoint" "acr_private_endpoint" {
+  count               = var.enable_private_endpoint && var.sku == "Premium" ? 1 : 0
+  name                = var.container_registry_name
+  location            = var.acr_location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+  tags                = var.tag_map
+  private_dns_zone_group {
+    name                 = "container-registry-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dnszoneacr.0.id]
+  }
+
+  private_service_connection {
+    name                           = "containerregistryprivatelink"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_container_registry.ContainerRegistry.id
+    subresource_names              = ["registry"]
+  }
+}
+
+resource "azurerm_private_dns_zone" "dnszoneacr" {
+  count               = var.existing_private_dns_zone == null && var.enable_private_endpoint && var.sku == "Premium" ? 1 : 0
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group_name
+  tags                = var.tag_map
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vent-link1" {
+  count                 = var.existing_private_dns_zone == null && var.enable_private_endpoint && var.sku == "Premium" ? 1 : 0
+  name                  = "vnet-private-zone-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.dnszoneacr.0.name
+  virtual_network_id    = var.virtual_network_id
+  registration_enabled  = true
+  tags                  = var.tag_map
+}
+
+############################## Resource Lock ##################################
+
 resource "azurerm_management_lock" "primary_lock" {
   count      = var.lock_level_value == "" ? 0 : 1
   name       = "${var.container_registry_name}-lock"
