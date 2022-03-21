81a0c3249708a5705b89a0d18fc15b5e30f786e0
S Aishwarya <s.aishwarya@opstree.com>
2022-03-21 08:45:28 +0000

    This Example is for creating ACR Module with advanced features.

--- example/advance/main.tf
+++ example/advance/main.tf
@@ -3,23 +3,23 @@ provider "azurerm" {
 }
 
 data "azurerm_virtual_network" "virtual_network" {
-  name                = "example-vnet" #Enter Virtual Network Name
+  name                = "example-vnet"          #Enter Virtual Network Name
   resource_group_name = "example-resourcegroup" #Enter Resource Group Name
 }
 
 data "azurerm_subnet" "subnet_acr" {
-  name    = "example-subnet-name" #Enter Subnet Name present in Virtual Network
-  resource_group_name = "example-subnet" #Enter Resource Group Name
+  name                 = "example-subnet-name"   #Enter Subnet Name present in Virtual Network
+  resource_group_name  = "example-subnet"        #Enter Resource Group Name
   virtual_network_name = "example-resourcegroup" #Enter Virtual Network Name
 }
 
 data "azurerm_user_assigned_identity" "userassignedid" {
-  name                = "userassignedid-name" #Enter ManagedID Name
+  name                = "userassignedid-name"   #Enter ManagedID Name
   resource_group_name = "example-resourcegroup" #Enter Resource Group Name
 }
 
 data "azurerm_key_vault" "key_vault" {
-  name                = "example-keyvault" #Enter Key Vault Name
+  name                = "example-keyvault"      #Enter Key Vault Name
   resource_group_name = "example-resourcegroup" #Enter Resource Group Name
 }
 
@@ -29,29 +29,33 @@ data "azurerm_key_vault_key" "keyvalue" {
 }
 
 module "acr" {
-  source                  = "../../"
-  container_registry_name = "examplexmplregistry"
-  resource_group_name     = "example-resourcegroup"
-  acr_location            = "eastus"
-  sku                     = "Premium"
-  admin_enabled           = true
-  public_network_access   = true
-  trust_policy            = true
-  zone_redundancy_enabled = true
-  georeplications         = ["centralus","centralindia"]
-  retention               = null
-  identity                = "UserAssigned"
-  identity_id             = data.azurerm_user_assigned_identity.userassignedid.id
-  encryption              = true
-  key_vault_key_id        = data.azurerm_key_vault_key.keyvalue.id
-  identity_client_id      = data.azurerm_user_assigned_identity.userassignedid.client_id
-  data_endpoint_enabled   = null
-  anonymous_pull_enabled  = null 
-  quarantine_policy_enabled = null
+  source                     = "../../"
+  container_registry_name    = "examplexmplregistry"
+  resource_group_name        = "example-resourcegroup"
+  acr_location               = "eastus"
+  sku                        = "Premium"
+  admin_enabled              = true
+  public_network_access      = true
+  trust_policy               = true
+  zone_redundancy_enabled    = true
+  georeplications            = ["centralus", "centralindia"]
+  retention                  = null
+  identity                   = "UserAssigned"
+  identity_id                = data.azurerm_user_assigned_identity.userassignedid.id
+  encryption                 = true
+  key_vault_key_id           = data.azurerm_key_vault_key.keyvalue.id
+  identity_client_id         = data.azurerm_user_assigned_identity.userassignedid.client_id
+  data_endpoint_enabled      = true
+  anonymous_pull_enabled     = true
+  quarantine_policy_enabled  = true
   network_rule_bypass_option = "AzureServices"
-  enable_private_endpoint = true
-  existing_private_dns_zone = null
-  virtual_network_id = data.azurerm_virtual_network.virtual_network.id
-  subnet_id = data.azurerm_subnet.subnet_acr.id
-  lock_level_value        = ""
+  enable_private_endpoint    = true
+  existing_private_dns_zone  = null
+  virtual_network_id         = data.azurerm_virtual_network.virtual_network.id
+  subnet_id                  = data.azurerm_subnet.subnet_acr.id
+  lock_level_value           = ""
+  tag_map = {
+    "env"        = "environment-name"
+    "created by" = "example"
+  }
 }
