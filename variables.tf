8c29028618da2014f5224efd10c1ef59fb9b1e67
S Aishwarya <s.aishwarya@opstree.com>
2022-03-21 06:06:22 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,8 +19,8 @@ variable "acr_location" {
 }
 variable "sku" {
   type        = string
-  default     = "Basic"
   description = "which SKU do you want (options: Basic,Standard,Premium) "
+  default     = "Basic"
   validation {
     condition     = contains(["Basic", "Standard", "Premium"], var.sku)
     error_message = "Argument 'sku' must one of 'Basic', 'Standard', or 'Premium'."
@@ -45,6 +45,37 @@ variable "public_network_access" {
   }
   default = true
 }
+
+variable "anonymous_pull_enabled" {
+  type        = bool
+  description = "Anonymous Pull access: true or false"
+  validation {
+    condition     = contains(["true", "false"], var.anonymous_pull_enabled)
+    error_message = "Argument 'anonymous pull access' must one of 'True' or 'False'."
+  }
+  default = false
+}
+
+variable "data_endpoint_enabled" {
+  type        = bool
+  description = "Want to enable Data Endpoint ? : true or false"
+  validation {
+    condition     = contains(["true", "false"], var.data_endpoint_enabled)
+    error_message = "Argument 'Data Endpoint Enabled' must one of 'True' or 'False'."
+  }
+  default = false
+}
+
+variable "quarantine_policy_enabled" {
+  type        = bool
+  description = "Quarantine policy access enabled : true or false"
+  validation {
+    condition     = contains([true, false], var.quarantine_policy_enabled)
+    error_message = "Argument 'Quarantine policy access' must one of 'True' or 'False'."
+  }
+  default = false
+}
+
 variable "retention" {
   type        = map(object({ enabled = bool, days = number }))
   description = "The number of days to retain an untagged manifest after which it gets purged."
@@ -60,8 +91,8 @@ variable "trust_policy" {
   default = false
 }
 variable "georeplications" {
-  description = "A list of Azure locations where the container registry should be geo-replicated.Location is to be added here"
   type        = list(string)
+  description = "A list of Azure locations where the container registry should be geo-replicated.Location is to be added here"
   default     = []
 }
 variable "zone_redundancy_enabled" {
@@ -110,4 +141,38 @@ variable "identity_client_id" {
 variable "lock_level_value" {
   type        = string
   description = "(Required) Specifies the Level to be used for this Lock. Possible values are `Empty (no lock)`, `CanNotDelete` and `ReadOnly`. Changing this forces a new resource to be created"
+}
+
+variable "enable_private_endpoint" {
+  type        = bool
+  description = "Manages a Private Endpoint to Azure Container Registry"
+  default     = false
+  validation {
+    condition     = contains([true, false], var.enable_private_endpoint)
+    error_message = "Argument 'Zone Redundancy Enabled' must one of 'True' or 'False'."
+  }
+}
+
+variable "existing_private_dns_zone" {
+  type        = any
+  description = "Name of the existing private DNS zone"
+  default     = null
+}
+
+variable "virtual_network_id" {
+  type        = string
+  description = "The ID of the virtual network (virtual network Region and ACR location must be same)"
+  default     = ""
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet (Subnet Region and ACR location must be same)"
+  default     = ""
+}
+
+variable "network_rule_bypass_option" {
+  type        = string
+  description = "Change network rule bypass option : Possible Values AzureServices or None"
+  default     = "AzureServices"
 }
