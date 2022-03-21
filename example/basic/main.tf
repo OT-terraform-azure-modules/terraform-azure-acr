20b2aa344bb268dde9bdac0495eba48781f3dca0
S Aishwarya <s.aishwarya@opstree.com>
2022-03-21 08:54:20 +0000

    Update main.tf

--- example/basic/main.tf
+++ example/basic/main.tf
@@ -11,29 +11,26 @@ module "res_group" {
 }
 
 module "acr" {
-  source                  = "../../"
-  container_registry_name = "examplexmpregistry"
-  resource_group_name     = module.res_group.resource_group_name
-  acr_location            = "east us"
-  sku                     = "Basic"
-  admin_enabled           = false
-  public_network_access   = true
-  trust_policy            = false
-  zone_redundancy_enabled = false
-  georeplications         = []
-  retention               = null
-  identity                = "SystemAssigned"
-  identity_id             = null
-  encryption              = null
-  key_vault_key_id        = null
-  identity_client_id      = null
-  quarantine_policy_enabled = null
-  anonymous_pull_enabled = null
-  data_endpoint_enabled = null
-  enable_private_endpoint = null
-  existing_private_dns_zone = null
-  virtual_network_id =""
-  subnet_id=""
+  source                     = "../"
+  container_registry_name    = "examplexmpregistry"
+  resource_group_name        = module.res_group.resource_group_name
+  acr_location               = "east us"
+  sku                        = "Basic"
+  admin_enabled              = false
+  public_network_access      = true
+  trust_policy               = false
+  zone_redundancy_enabled    = false
+  georeplications            = []
+  retention                  = null
+  identity                   = "SystemAssigned"
+  identity_id                = null
+  encryption                 = null
+  key_vault_key_id           = null
+  identity_client_id         = null
   network_rule_bypass_option = "AzureServices"
-  lock_level_value        = ""
+  lock_level_value           = ""
+  tag_map = {
+    "env"        = "environment-name"
+    "created by" = "example"
+  }
 }
