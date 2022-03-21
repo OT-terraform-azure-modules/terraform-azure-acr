a54a9946fe083278ab462aa329481dc3117fedaa
S Aishwarya <s.aishwarya@opstree.com>
2022-03-21 06:58:10 +0000

    Update main.tf

--- example/basic/main.tf
+++ example/basic/main.tf
@@ -27,5 +27,13 @@ module "acr" {
   encryption              = null
   key_vault_key_id        = null
   identity_client_id      = null
+  quarantine_policy_enabled = null
+  anonymous_pull_enabled = null
+  data_endpoint_enabled = null
+  enable_private_endpoint = null
+  existing_private_dns_zone = null
+  virtual_network_id =""
+  subnet_id=""
+  network_rule_bypass_option = "AzureServices"
   lock_level_value        = ""
 }
